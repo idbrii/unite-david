da87ce19b551b3e87af23e1afe3415f9595d42b4
David Briscoe <idbrii@gmail.com>
2015-03-03 11:19:48 -0800

    Consistently indent walkup with spaces
    It was using a mix.

--- bin/walkup.py
+++ bin/walkup.py
@@ -18,38 +18,38 @@ import os
 from os import path
 
 def walk_up(bottom):
-	""" 
-	mimic os.walk, but walk 'up'
-	instead of down the directory tree
-	"""
+    """
+    mimic os.walk, but walk 'up'
+    instead of down the directory tree
+    """
 
-	bottom = path.realpath(bottom)
+    bottom = path.realpath(bottom)
 
-	#get files in current dir
-	try:
-		names = os.listdir(bottom)
-	except Exception as e:
-		print e
-		return
+    #get files in current dir
+    try:
+        names = os.listdir(bottom)
+    except Exception as e:
+        print e
+        return
 
 
-	dirs, nondirs = [], []
-	for name in names:
-		if path.isdir(path.join(bottom, name)):
-			dirs.append(name)
-		else:
-			nondirs.append(name)
+    dirs, nondirs = [], []
+    for name in names:
+        if path.isdir(path.join(bottom, name)):
+            dirs.append(name)
+        else:
+            nondirs.append(name)
 
-	yield bottom, dirs, nondirs
+    yield bottom, dirs, nondirs
 
-	new_path = path.realpath(path.join(bottom, '..'))
-	
-	# see if we are at the top
-	if new_path == bottom:
-		return
+    new_path = path.realpath(path.join(bottom, '..'))
 
-	for x in walk_up(new_path):
-		yield x
+    # see if we are at the top
+    if new_path == bottom:
+        return
+
+    for x in walk_up(new_path):
+        yield x
 
 if __name__ == '__main__':
     # tests/demos
@@ -61,9 +61,9 @@ if __name__ == '__main__':
 
     # look for a TAGS file above the
     # current directory
-	for c,d,f in walk_up(os.curdir):
-		if 'TAGS' in f:
-			print c
-			break
+    for c,d,f in walk_up(os.curdir):
+        if 'TAGS' in f:
+            print c
+            break
 
-	
+
