8d8f702d51e6725af549e549707b0d5cc15e24e1
David Briscoe <idbrii@gmail.com>
2015-12-22 16:23:59 -0800

    Make a copy of filelist opener for emacs
    This one just gives the file names.
    I should make that an argument instead.

---
+++ bin/projectileopenfilelist.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import os
+
+import walkup
+
+
+index = 'filelist'
+for root, dirs, files in walkup.walk_up(os.getcwd()):
+    if index in files:
+        with open(os.path.join(root, index), 'r') as f:
+            for line in f:
+                line = line.strip()
+                print(line, end='\0')
